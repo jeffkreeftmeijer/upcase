bedb37f0ac42921f0ee60afd24d34113d25fca04
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-06-26 21:02:33 +0200

    Implement upcase::upcase()

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,17 @@
+use std::io::{Error, Read, Write};
+
+pub fn upcase(
+    input: &mut impl Read,
+    output: &mut impl Write,
+) -> Result<(), Error> {
+    let mut buffer = "".to_string();
+
+    input.read_to_string(&mut buffer)?;
+    output.write_all(buffer.to_uppercase().as_bytes())?;
+
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
