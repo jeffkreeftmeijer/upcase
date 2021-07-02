a592349fa4922ecda8cced8a8f28c185e78c3b33
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-07-02 19:52:06 +0200

    Add assert_utf8_bytes_eq! macro

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,23 @@
 mod tests {
     use super::*;
 
+    macro_rules! assert_utf8_bytes_eq {
+        ($left:expr, $right:expr) => {
+            if ($left != $right) {
+                panic!(
+                    "assertion failed: `(left == right)`\n  left: `{:?}`\n right: `{:?}`",
+                    String::from_utf8_lossy($left),
+                    String::from_utf8_lossy($right),
+                )
+            }
+        };
+    }
+
     #[test]
     fn writes_upcased_input_to_output() {
         let mut output: Vec<u8> = Vec::new();
 
-        upcase(&mut "Hello, world!\n".as_bytes(), &mut output).unwrap();
-        assert_eq!(&output, b"HELLO, WORLD!\n");
+        upcase(&mut "uppercase!\n".as_bytes(), &mut output).unwrap();
+        assert_utf8_bytes_eq!(&output, b"UPPERCASE!\n");
     }
 }
