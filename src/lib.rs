da076d6a8c53fbb78e6833b1d40d6b4736a1cd41
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-12 09:20:00 +0200

    Tangle code from source blocks
    [date skip]

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
-mod tests {
-    use super::*;
-
-    #[test]
-    fn writes_upcased_input_to_output() {
-        let mut output: Vec<u8> = Vec::new();
-
-        upcase(&mut "Hello, world!\n".as_bytes(), &mut output).unwrap();
-        assert_eq!(&output, b"HELLO, WORLD!\n");
-    }
-}
-
 use std::io::{Error, Read, Write};
 
 pub fn upcase(
@@ -22,4 +10,17 @@ pub fn upcase(
     output.write_all(buffer.to_uppercase().as_bytes())?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn writes_upcased_input_to_output() {
+        let mut output: Vec<u8> = Vec::new();
+
+        upcase(&mut "Hello, world!\n".as_bytes(), &mut output).unwrap();
+        assert_eq!(&output, b"HELLO, WORLD!\n");
+    }
 }
