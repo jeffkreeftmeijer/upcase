dbdf7405d3e6179aaa6d203c92a106bc50c5923d
Jeff Kreeftmeijer <jeff@kreeft.me>
2023-05-12 09:19:00 +0200

    Update indentation in code blocks
    [date skip]

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+mod tests {
+    use super::*;
+
+    #[test]
+    fn writes_upcased_input_to_output() {
+        let mut output: Vec<u8> = Vec::new();
+
+        upcase(&mut "Hello, world!\n".as_bytes(), &mut output).unwrap();
+        assert_eq!(&output, b"HELLO, WORLD!\n");
+    }
+}
+
 use std::io::{Error, Read, Write};
 
 pub fn upcase(
@@ -10,29 +22,4 @@ pub fn upcase(
     output.write_all(buffer.to_uppercase().as_bytes())?;
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    macro_rules! assert_utf8_bytes_eq {
-        ($left:expr, $right:expr) => {
-            if ($left != $right) {
-                panic!(
-                    "assertion failed: `(left == right)`\n  left: `{:?}`\n right: `{:?}`",
-                    String::from_utf8_lossy($left),
-                    String::from_utf8_lossy($right),
-                )
-            }
-        };
-    }
-
-    #[test]
-    fn writes_upcased_input_to_output() {
-        let mut output: Vec<u8> = Vec::new();
-
-        upcase(&mut "uppercase!\n".as_bytes(), &mut output).unwrap();
-        assert_utf8_bytes_eq!(&output, b"UPPERCASE!\n");
-    }
 }
